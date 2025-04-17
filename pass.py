c740b9c9180ade6f2ee2ca547b8669be06289602
adelinakim <adelin4ik91@gmail.com>
2025-04-17 21:05:09 +0500

    Решила задание
    1) Внутри функции записала все квадраты чисел в list и возвращайте его при завершении функции.
    2) Во втором вызове функции вывела возвращенный список.

--- pass.py
+++ pass.py
@@ -1,8 +1,10 @@
 def processing (text: str, flag: bool, number: float):
-      try:
-      if text = "":
-        raise ValueError("Строка не может быть пустой")
-        except
+    try:
+        if text == "":
+            raise valueError("Строка не может быть пустой")
+    except:
+        pass
+    spisok = []
     for num in range(0, int(number)):
         square = num ** 2
         if num == 4:
@@ -12,10 +14,11 @@ def processing (text: str, flag: bool, number: float):
         if square > 50:
             print(f"Квадрат числа {num} больше 50")
         if 20 <= square <= 50:
-            print(f"Квадрат числа {i} больше или равен 20 и меньше или равен 50")
+            print(f"Квадрат числа {num} больше или равен 20 и меньше или равен 50")
         else:
-            print(f"Квадрат числа {i} меньше 20")
-
+            print(f"Квадрат числа {num} меньше 20")
         print(f"Квадрат числа {num} = {square}")
+        spisok.append(square) 
+        return spisok
 processing ("sample text", True, 10.0)
 processing ("pink", False, 4.4)
