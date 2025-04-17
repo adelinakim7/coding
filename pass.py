ed15de13790f69fba84689eab8d4730b3516b098
adelinakim <adelin4ik91@gmail.com>
2025-04-17 20:42:12 +0500

    Решила ряд заданий
    1) В цикл добавила вывод сообщения "Квадрат числа {number} больше или равен 20 и меньше или равен 50".
    
    2) В цикл добавила вывод сообщения "Квадрат числа {number} меньше 20".
    
    3) В функцию добавила проверку значения str на пустоту и если оно пусто вызывать исключение valueError.

--- pass.py
+++ pass.py
@@ -1,4 +1,8 @@
 def processing (text: str, flag: bool, number: float):
+      try:
+      if text = "":
+        raise ValueError("Строка не может быть пустой")
+        except
     for num in range(0, int(number)):
         square = num ** 2
         if num == 4:
@@ -7,7 +11,11 @@ def processing (text: str, flag: bool, number: float):
             break 
         if square > 50:
             print(f"Квадрат числа {num} больше 50")
+        if 20 <= square <= 50:
+            print(f"Квадрат числа {i} больше или равен 20 и меньше или равен 50")
+        else:
+            print(f"Квадрат числа {i} меньше 20")
+
         print(f"Квадрат числа {num} = {square}")
 processing ("sample text", True, 10.0)
 processing ("pink", False, 4.4)
-
