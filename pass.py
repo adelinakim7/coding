c6c24518648158f7b0c5ed57f4d17f244d6a22d6
adelinakim <adelin4ik91@gmail.com>
2025-04-17 20:21:21 +0500

    Решила 3 задачи
     В цикл добавила пропуск числа 4 и завершение цикла на числе 9.
    
    2) В цикл добавила вывод сообщения "Квадрат числа {number} больше 50".
    
    3) Добавила второй вызов функции, передавая другие значения.

--- pass.py
+++ pass.py
@@ -1,5 +1,13 @@
-def processing (text: str, flag: bool, number float):
-    for num in range(0, number)
-    square = num * * 2
+def processing (text: str, flag: bool, number: float):
+    for num in range(0, int(number)):
+        square = num ** 2
+        if num == 4:
+            continue  
+        if num == 9:
+            break 
+        if square > 50:
+            print(f"Квадрат числа {num} больше 50")
         print(f"Квадрат числа {num} = {square}")
 processing ("sample text", True, 10.0)
+processing ("pink", False, 4.4)
+
