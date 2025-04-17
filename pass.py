fb714a23d5d497f2262a93492e3668009e23dcf3
adelinakim <adelin4ik91@gmail.com>
2025-04-17 19:53:35 +0500

    Изменила передаваемую функцию значение типа float на 10.0
     Внутри функции цикл перебирающий все целочисленные значения от нуля до принятого значения float и написала внутри цикла вывод квадрата текущего числа.

--- pass.py
+++ pass.py
@@ -1,3 +1,5 @@
-def processing (text: str, flag: bool, number float)
-    pass
-processing ("sample text", True, 3.14)
+def processing (text: str, flag: bool, number float):
+    for num in range(0, number)
+    square = num * * 2
+        print(f"Квадрат числа {num} = {square}")
+processing ("sample text", True, 10.0)
