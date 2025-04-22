c07e1ddda3fa7c7e16b388c743dc1d950044d023
adelinakim <adelin4ik91@gmail.com>
2025-04-22 21:16:54 +0500

    Работала с кодом
    Написала функцию для поиска товара с наибольшей общей стоимостью (цена × количество) и выведите его название и сумму.

--- products.py
+++ products.py
@@ -7,8 +7,18 @@ def print_cheap_products(products):
     name, price = product
     if price < 80:
         print(name)
+        
 def compute():     
     for name, price, quantity in products:
         result = price * quantity
         print(f"{name} — всего на {result} рублей")
 compute()
+
+max_product = None
+max_value = 0
+for name, price, quantity in products:
+        total = price * quantity
+        if total > max_value:
+            max_value = total
+            max_product = name
+print(f"Самый ценный товар: {max_product} — {max_value} рублей")
