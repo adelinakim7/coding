36fcfb877a1cf7789895d6a2e8a2d5ef6063fe03
adelinakim <adelin4ik91@gmail.com>
2025-04-29 20:01:18 +0500

    Работа с файлом

--- append.py
+++ append.py
@@ -1,3 +1,10 @@
 def append_to_file(filename):
     with open(filename, 'a') as file:
         file.write("Hi.\n")
+         with open('file.name', 'r') as file:
+            print(file.reading())
+             
+def reading(filename):
+    try:
+        with open('file.txt', 'r') as file:
+            print(file.reading())
