136425efccb074a50a8850337afee94e98681003
adelinakim <adelin4ik91@gmail.com>
2025-05-06 20:21:12 +0500

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -5,3 +5,7 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+numbers = [27, 77, 444, 52, 67]
+print("Исходный список чисел:", numbers)
+sorted_numbers = buble_sort(numbers)
+print("отсортированный массив: ", sorted_numbers)
