84f81085c8deca3e7b4946a6c8594f799de2a163
adelinakim <adelin4ik91@gmail.com>
2025-06-05 20:30:53 +0500

    ооп
    В классе Student добавила атрибут grades — пустой список.
    Добавила метод add_grade(self, grade), который добавляет оценку в список.
    Добавила метод get_average(self) — возвращает среднюю оценку.

--- education.py
+++ education.py
@@ -2,8 +2,15 @@ class Student:
     def __init__(self, name, student_id):
         self.name = name
         self.student_id = student_id
+        self.grades = []
     def  display_info(self):
         print(f"Имя: {my_student.name}, ID: {my_student.student_id}")
+    def add_grade(self, grade):
+        self.grades.append(grade)
+    def get_average(self):
+        if not self.grades:
+            return 0.0
+        return sum (self.grades)/len (self.grades) 
     
 my_student = Student("Розанна Пак", "444")
 my_student.display_info()
@@ -13,9 +20,18 @@ class Group:
         self.student = []
     def add_student(self, student):
         self.student.append(student)
+    def show_students(self):
+        if not self.students:
+            print("Группа пуста.")
+        else:
+            for student in self.students:
+                student.display_info()    
     
 group = Group()
 print(group.student)
 group.add_student(my_student)
 for student in group.student:
     my_student.display_info()
+student.add_grade(5)
+student.add_grade(4)
+print(f"Средняя оценка: {student.get_average():.2f}")
