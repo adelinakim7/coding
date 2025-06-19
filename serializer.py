21bc419a8c52f6d5281c1f86bff0861844a06f3d
adelinakim <adelin4ik91@gmail.com>
2025-06-19 21:31:15 +0500

    Сериализация
    Вне контекста классов написала бесконечный цикл, который будет запрашивать у пользователя следующее действие.
    
    Реализовала механизм выбора из следующих пунктов: 'добавить/удалить/вывести список книг' и вызова соответствующих методов.
    
    Реализовала ранее созданные методы добавить/удалить/вывести в классе library

--- serializer.py
+++ serializer.py
@@ -1,37 +1,39 @@
-class Book:
-    def __init__(self, name, author, year):
-        self.name = name
+class Book():
+    def __init__(self, title, author, year):
+        self.title = title
         self.author = author
         self.year = year
 
-    def get_info(self):
-        print(f'{self.name}, {self.author}, {self.year}')
+    def __str__(self):
+        return f"{self.title} ({self.author}, {self.year})"
+
 
 class Library:
-  def __init__(self):
-    self.books = []
+    def __init__(self):
+        self.books = []
 
-  def add_book(self, book):
-    self.books.append(book)
-    print(f"Книга '{book.name}' добавлена в библиотеку")
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Книга '{book.title}' добавлена.")
 
-  def remove_book(self, book_name):
-    for book in self.books:
-      if book.name == book_name:
-        self.books.remove(book)
-        print(f"Книга '{book_name}' удалена из библиотеки")
-        return
-      print(f"Книга '{book_name}' не найдена")
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title.lower() == title.lower():
+                self.books.remove(book)
+                print(f"Книга '{title}' удалена.")
+                return
+        print(f"Книга '{title}' не найдена.")
 
-  def show_all_books(self):
-    if not self.books:
-      print("В библиотеке нет книг")
-      return
-    print("Список книг в библиотеке:")
-    for book in self.books:
-      book.get_info()
-      
+    def show_all_books(self):
+        if self.books:
+            print("Список книг:")
+            for i, book in enumerate(self.books, 1):
+                print(f"{i}. {book}")
+        else:
+            print("Список книг пуст.")
+
 my_library = Library()
+
 book1 = Book("Большая книга серийных убийц", "Джек Роузвуд", 2024)
 book2 = Book("Ромео и Джульетта", "Уильям Шекспир", 1595)
 book3 = Book("Гамлет", "Уильям Шекспир", 1603)
@@ -45,3 +47,32 @@ my_library.show_all_books()
 my_library.remove_book("Большая книга серийных убийц")
 
 my_library.show_all_books()
+
+
+while True:
+    action = input("\nВыберите действие (добавить/удалить/вывести/выход): ").strip().lower()
+
+    if action == 'добавить':
+        title = input("Введите название книги: ").strip()
+        author = input("Введите автора книги: ").strip()
+        try:
+            year = int(input("Введите год издания: ").strip())
+        except ValueError:
+            print("Некорректный ввод года.")
+            continue
+        book = Book(title, author, year)
+        my_library.add_book(book)
+
+    elif action == 'удалить':
+        title = input("Введите название книги для удаления: ").strip()
+        my_library.remove_book(title)
+
+    elif action == 'вывести':
+        my_library.show_all_books()
+
+    elif action == 'выход':
+        print("Выход из программы.")
+        break
+
+    else:
+        print("Неизвестное действие. Повторите ввод.")
