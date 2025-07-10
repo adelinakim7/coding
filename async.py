43ef441be7cf59e9d54dcdece6103fb3a0a1ca32
adelinakim <adelin4ik91@gmail.com>
2025-07-10 20:20:01 +0500

    Асинхронность
    Переписала код используя модуль asyncio чтобы задавать индивидуальное время вывода каждого слова.

--- async.py
+++ async.py
@@ -1,7 +1,20 @@
-import time
-words = ["Charli", "Addison", "Payton", "Riley", "Nick", "Nessa", "Jayden", "Madison", "Josh", "Eva"]
-x = 0
-while x <= 10:
-    time.sleep(1)
-    print(words[x])
-    x += 1
+import asyncio
+
+async def Hi(word, delay):
+    await asyncio.sleep(delay)
+    print(word)
+    
+async def main():
+    await asyncio.gather(
+        Hi("Charli", 5),
+        Hi("Addison", 7),
+        Hi("Payton", 5),
+        Hi("Riley", 4),
+        Hi("Nick", 1),
+        Hi("Nessa", 9),
+        Hi("Jayden", 3),
+        Hi("Madison", 6),
+        Hi("Josh", 8),
+        Hi("Eva", 2),
+        )
+asyncio.run(main())
