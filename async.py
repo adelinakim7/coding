1b954215a96747d65b1302b1c3eeee98f41412c0
adelinakim <adelin4ik91@gmail.com>
2025-07-10 19:44:49 +0500

    Асинхронность
    Написала программу, которая выводит 10 слов одно за другим с паузой в одну секунду.

---
+++ async.py
@@ -0,0 +1,7 @@
+import time
+words = ["Charli", "Addison", "Payton", "Riley", "Nick", "Nessa", "Jayden", "Madison", "Josh", "Eva"]
+x = 0
+while x <= 10:
+    time.sleep(1)
+    print(words[x])
+    x += 1
